531fe10210277c34119335a13c07afd2d9f6ed58
Aleksander Burkow <aleksanderburkow@gmail.com>
2012-04-21 01:51:02 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tidlaus::Application.config.secret_token = 'be23d041b501bbbba253b9571f900148aafc1e4eda3ccb6f97e0e32ea6cdf8ed4df3203585346b0dc22f831ea9b729822eb57c95e122bde0ce0ced6d7c1c7b30'
