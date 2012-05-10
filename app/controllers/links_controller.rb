721c6908ea7cdef0f5c05ffc628c5eb34ad7d470
Aleksander Burkow <aleksanderburkow@gmail.com>
2012-05-10 01:50:25 +0000

    fixa header

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,23 +1,27 @@
 class LinksController < ApplicationController
 
 	def index
+        @title = "Lenkeforlengaren"
 		@links = Link.all
 	end
 
 	def show
+        @title = "Lenkeforlengaren"
 		@link = Link.find_by_longurl(params[:id])
 		redirect_to urlhelper(@link)
 		#redirect_to #'#{@link.shorturl}'
 	end
 
 	def new
+        @title = "Lenkeforlengaren"
 		@link = Link.new
 	end
 
 	def create
+        @title = "Lenkeforlengaren"
 		@link = Link.create(params[:link])
 		if @link.save
-			redirect_to new_link_path, :flash => { :success => "Suksess! Du skapte en lang link: www.tidla.us/#{@link.longurl} Denne gaar til #{@link.shorturl}" }
+			redirect_to new_link_path, :flash => { :success => "Suksess! Du skapte en lang link: www.tidla.us/#{@link.longurl} Denne gaar til #{urlhelper(@link)}" }
 		else
 			flash[:failure] = "Feilformatert Link :("
 			render 'new'
