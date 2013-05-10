4a6ade58bb9d0a4fb2c7767d8a0147db35504f1f
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-05-10 04:28:04 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,59 +9,57 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130317014827) do
+ActiveRecord::Schema.define(version: 20130510010602) do
 
-  create_table "articles", :force => true do |t|
+  create_table "articles", force: true do |t|
     t.string   "title"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text     "content"
     t.boolean  "html_safe"
   end
 
-  create_table "images", :force => true do |t|
+  create_table "images", force: true do |t|
     t.string   "title"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.text     "description"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
   end
 
-  create_table "links", :force => true do |t|
+  create_table "links", force: true do |t|
     t.string   "shorturl"
     t.string   "longurl"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-    t.integer  "views",      :default => 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "views",      default: 0
   end
 
-  create_table "roles", :force => true do |t|
+  create_table "roles", force: true do |t|
     t.string   "title"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", :force => true do |t|
+  create_table "roles_users", force: true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  create_table "users", force: true do |t|
     t.string   "name"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "salt"
     t.string   "encrypted_password"
     t.string   "email"
-  end
-
-  create_table "users_roles", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "role_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
