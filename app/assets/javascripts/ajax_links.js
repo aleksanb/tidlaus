7ad5e6f0e717436783ee16c85fb1cb2c78a3f4cb
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-05-10 04:39:32 +0200

    remove ajax links in favor of turbolinks

--- app/assets/javascripts/ajax_links.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $("a").on( "click" , function() {
-        var href = $(this).attr("href");
-        $.get(href, function(data) {
-            $("#maincontainer").html(data);
-        });
-        event.preventDefault();
-    });
-});
