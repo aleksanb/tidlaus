23fc038bc08fc83e22e7b4dded753f5c0661c0e0
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-05-14 05:45:10 +0000

    commited out old auth code, before removing it later

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   filter_access_to :all
 
   def new
-  	@title = "Sign in"
+    @title = "Sign in"
   end
 
   def create_omniauth
@@ -21,27 +21,27 @@ class SessionsController < ApplicationController
     end
   end
 
-  def create
+  # def create
 
-    user = User.authenticate(session_params)
-  	if user.nil? 
-  		flash.now[:failure] = "<span class='black'>Invalid</span> login details.".html_safe
-  		render 'new'
-  	else
-  		session[:user_id] = user.id
-  		flash[:login] = "Logged in as <span class='black'>#{user.name}.</span>".html_safe
-  		redirect_to user
-  	end
-  end
+  #   user = User.authenticate(session_params)
+  # 	if user.nil? 
+  # 		flash.now[:failure] = "<span class='black'>Invalid</span> login details.".html_safe
+  # 		render 'new'
+  # 	else
+  # 		session[:user_id] = user.id
+  # 		flash[:login] = "Logged in as <span class='black'>#{user.name}.</span>".html_safe
+  # 		redirect_to user
+  # 	end
+  # end
 
   def destroy
-  	session[:user_id] = nil
-  	flash[:success] = "Logged <span class='black'>out.</span>".html_safe
+    session[:user_id] = nil
+    flash[:success] = "Logged <span class='black'>out.</span>".html_safe
     redirect_to root_path
   end
 
-  private
-    def session_params
-      params.require(:session).permit(:name, :password)
-    end
+  # private
+  #   def session_params
+  #     params.require(:session).permit(:name, :password)
+  #   end
 end
